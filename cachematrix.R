a2d1e02b7d8705617eda51829a2be1ca03f68dec
celsofaf <celsofaf@gmail.com>
2014-04-25 23:12:06 -0300

    Commiting for first time

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The two functions below are used to set up a matrix and set/get its
+## inverse. If the inverse is already calculated, the second function,
+## which is suposed to calculate the inverse, gets the already calcu-
+## lated inverse, which is cached on memory.
+##
+## These functions assume that the matrix is already square and inver-
+## tible, that is, nonsingular. There are no tests to certify for it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## This function creates a special "matrix", which is really a list
+    ## containing a function to:
+    ## 1. set the value of the matrix - makeCacheMatrix$set()
+    ## 2. get the value of the matrix - makeCacheMatrix$get()
+    ## 3. set the value of the inverse - makeCacheMatrix$setinv()
+    ## 4. get the value of the inverse - makeCacheMatrix$setinv()
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y     # x is defined outside; is the argument to the main function
+        m <<- NULL  # m is defined outside
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv  # m is defined outside this function
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    ## This function calculates the inverse of the special "matrix"
+    ## created with makeCacheMatrix(). However, it first checks to see
+    ## if the inverse has already been calculated. If so, it gets the
+    ## inverse from the cache and skips the computation. Otherwise, it
+    ## calculates the inverse of the data and sets the value of the in-
+    ## verse in the cache via the setinv function.
+    ## 
+    ## The matrix is assumed to be square and nonsingular.
+    
+    m <- x$getinv()
+    if(!is.null(m)) {  # gets the cached inverse; doesn't need to calculate
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)  # calculates the inverse
+    x$setinv(m)
+    m  # returns the calculated inverse
 }
